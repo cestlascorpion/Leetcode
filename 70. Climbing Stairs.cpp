fde297bb69c2469842abf0379a3dd7f31fc7964e
cestlascorpion <cestlascorpion@gmail.com>
2018-01-20 13:07:16 +0800

    70. Climbing Stairs

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,36 @@
+/* 70. Climbing Stairs
+ * 斐波那契数列 f(n) = f(n-2) + f(n-1)
+ * 到达第n阶的方法 = 到达第n-2阶的方法 + 到达第n-1阶的方法 f(1) = 1 f(2) = 2
+ * 递归耗时太长 
+ */
+class Solution {
+public:
+    int climbStairs(int n)
+    {
+        /*
+        int stepone = 0;
+        int steptwo = 1;
+        int sum = 0;
+        
+        for(int i = 0;i < n; i ++)//到达第n阶的方法 = 到达第n-1阶的方法 + 到达第n-2阶的方法 f(1) = 1 f(2) = 2 
+        {
+            sum = stepone + steptwo;
+            stepone = steptwo;
+            steptwo = sum;
+        }
+        
+        return sum;
+        */
+        if(n<=2)
+            return n;
+        int onestep = 1;
+        int twostep = 2;
+        for(int i = 3; i <= n; i ++)
+        {
+            int sum = onestep + twostep;
+            onestep = twostep;
+            twostep = sum;
+        }
+        return twostep;
+    }
+};
