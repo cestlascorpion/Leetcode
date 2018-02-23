51376015eb2b3036dbf326e2c3b631f5f9c0c62e
cestlascorpion <cestlascorpion@gmail.com>
2018-02-23 23:40:21 +0800

    406. Queue Reconstruction by Height

---
+++ 406. Queue Reconstruction by Height.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+  public:
+    static bool cmp(pair<int, int> a, pair<int, int> b)
+    {
+        if (a.first == b.first)
+            return a.second < b.second;
+        return a.first > b.first;
+    }
+
+    vector<pair<int, int>> reconstructQueue(vector<pair<int, int>> &people)
+    {
+        vector<pair<int, int>> ans;
+        ans.clear();
+        sort(people.begin(), people.end(), cmp);
+        for (auto i : people)
+            //此时前面的人一定都比后面的高所以他们的k就是他们应该在的位置
+            ans.insert(ans.begin() + i.second, i);
+        return ans;
+    }
+};
