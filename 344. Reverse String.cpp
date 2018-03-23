3430db5b136a050bbe9bc6e4adc501068af977a9
cestlascorpion <cestlascorpion@gmail.com>
2018-03-23 12:16:34 +0800

    A bad day for easy Problems

---
+++ 344. Reverse String.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+  public:
+    string reverseString(string s)
+    {
+        if (s.size() < 2)
+            return s;
+        int p1 = 0;
+        int p2 = s.size() - 1;
+        while (p1 < p2)
+            swap(s[p1++], s[p2--]);
+        return s;
+    }
+};
---
+++ 796. Rotate String.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+  public:
+    bool rotateString(string A, string B)
+    {
+        if (A.length() < 1 && B.length() < 1)
+            return true;
+        if (A.length() < 1 || B.length() < 1 || A.length() != B.length())
+            return false;
+        int len = A.length();
+        for (int i = 0; i < len; i++)
+        {
+            if (A[i] == B[0])
+            {
+                if (isSame(A, B, i, len))
+                    return true;
+            }
+        }
+        return false;
+    }
+    bool isSame(string &A, string &B, int index, int length)
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (A[(index + i) % length] != B[i])
+                return false;
+        }
+        return true;
+    }
+};
