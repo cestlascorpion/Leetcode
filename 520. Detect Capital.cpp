d3277838bd3d01eaf9bb05787fb0e013923d783a
cestlascorpion <cestlascorpion@gmail.com>
2018-03-23 23:06:42 +0800

    Detect Capital

---
+++ 520. Detect Capital.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+  public:
+    bool detectCapitalUse(string word)
+    {
+        int len = word.length();
+        if (len < 1)
+            return false;
+        if (len == 1)
+            return true;
+
+        if (word[0] >= 'A' && word[0] <= 'Z')
+        {
+            if (word[1] >= 'A' && word[1] <= 'Z')
+            {
+                for (int i = 2; i < len; i++)
+                    if (!(word[i] >= 'A' && word[i] <= 'Z'))
+                        return false;
+            }
+            else if (word[1] >= 'a' && word[1] <= 'z')
+            {
+                for (int i = 2; i < len; i++)
+                    if (!(word[i] >= 'a' && word[i] <= 'z'))
+                        return false;
+            }
+            else
+                return false;
+        }
+        else if (word[0] >= 'a' && word[0] <= 'z')
+        {
+            for (int i = 1; i < len; i++)
+                if (!(word[i] >= 'a' && word[i] <= 'z'))
+                    return false;
+        }
+        else
+            return false;
+        return true;
+    }
+};
