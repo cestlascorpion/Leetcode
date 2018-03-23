bfe909b5635ac210b986c3302fc760a776192bf5
cestlascorpion <cestlascorpion@gmail.com>
2018-03-23 19:51:37 +0800

    693. Binary Number with Alternating Bits

---
+++ 693. Binary Number with Alternating Bits.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+  public:
+    bool hasAlternatingBits(int n)
+    {
+        int flag = n & 1;
+        do
+        {
+            n = n >> 1;
+            if ((n & 1) == flag)
+                return false;
+            flag = n & 1;
+        } while (n);
+        return true;
+    }
+};
