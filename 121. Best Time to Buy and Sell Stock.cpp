3533584180d7da31d1376d6735851fc9e08c3e3d
cestlascorpion <cestlascorpion@gmail.com>
2018-03-25 19:22:49 +0800

    stock

--- 121. Best Time to Buy and Sell Stock.cpp
+++ 121. Best Time to Buy and Sell Stock.cpp
@@ -5,20 +5,6 @@ class Solution
   public:
     int maxProfit(vector<int> &prices)
     {
-        /*
-        int i, j;
-        int res = 0;
-        for(i = 0; i < prices.size(); i ++)
-        {
-            for(j = i + 1; j < prices.size(); j ++)
-            {
-                int delta = prices.at(j) - prices.at(i);
-                if(delta > res)
-                    res = delta;
-            }
-        }
-        return res;
-        */
         int res = 0;
         int min = INT_MAX;
         for (int i = 0; i < prices.size(); i++)
---
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,15 @@
+#include "0. config.h"
+
+class Solution
+{
+  public:
+    int maxProfit(vector<int> &prices)
+    {
+        int ans = 0;
+        if(prices.size()<2)
+            return ans;
+        for (int i = 0; i < prices.size() - 1; i++)
+            ans += (prices[i + 1] - prices[i] > 0 ? prices[i + 1] - prices[i] : 0);
+        return ans;
+    }
+};
