b6dd3c9120ca4553c3420690cbd03661ddbef60a
cestlascorpion <cestlascorpion@gmail.com>
2018-03-25 15:53:04 +0800

    717

---
+++ 717. 1-bit and 2-bit Characters.cpp
@@ -0,0 +1,22 @@
+#include "0. config.h"
+
+class Solution
+{
+  public:
+    bool isOneBitCharacter(vector<int> &bits)
+    {
+        int length = bits.size();
+        if (length <= 0)
+            return false;
+        int index = 0;
+        while (index < length)
+        {
+            if (bits[index] == '0' && index == length - 1)
+                return true;
+            if (bits[index] == '1')
+                index++;
+            index++;
+        }
+        return false;
+    }
+};
