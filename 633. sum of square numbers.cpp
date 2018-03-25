ad24cb9384f27892a2cd04b25786bd0a79a4f044
cestlascorpion <cestlascorpion@gmail.com>
2018-03-25 22:33:02 +0800

    two point

--- 633. sum of square numbers.cpp
+++ 633. sum of square numbers.cpp
@@ -5,8 +5,29 @@ class Solution
   public:
     bool judgeSquareSum(int c)
     {
+        int low = 0;
+        int high = floor(sqrt(c));
+        while (low <= high)
+        {
+            int sum = low * low + high * high;
+            if (sum == c)
+                return true;
+            else if (sum > c)
+                high--;
+            else
+                low++;
+        }
+        return false;
+    }
+};
+class Solution2
+{
+  public:
+    bool judgeSquareSum(int c)
+    {
         set<int> s;
-        for (int i = 0; i * i <= c; i++)
+        int root = floor(sqrt(c));
+        for (int i = 0; i <= root; i++)
             s.insert(i * i);
         for (set<int>::iterator it = s.begin(); it != s.end(); it++)
         {
