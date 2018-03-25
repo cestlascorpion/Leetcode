2febdc60948e9f6a12b124a23e9dccc1994078f7
cestlascorpion <cestlascorpion@gmail.com>
2018-03-25 16:06:31 +0800

    26进制

---
+++ 171. Excel Sheet Column Number.cpp
@@ -0,0 +1,19 @@
+#include "0. config.h"
+
+class Solution
+{
+  public:
+    int titleToNumber(string s)
+    {
+        if (s.length() <= 0)
+            return 0;
+        int ans = 0;
+        int base = 1;
+        for (int i = s.length() - 1; i >= 0; i--)
+        {
+            ans = (s[i] - 'A' + 1) * base + ans;
+            base = base * 26;
+        }
+        return ans;
+    }
+};
