00d54e72ff111bae97ae1e5c99e9842a4799c144
cestlascorpion <cestlascorpion@gmail.com>
2018-03-27 23:14:23 +0800

    set

---
+++ 217. Contains Duplicate.cpp
@@ -0,0 +1,13 @@
+#include "0. config.h"
+
+class Solution
+{
+  public:
+    bool containsDuplicate(vector<int> &nums)
+    {
+        if (nums.size() <= 0)
+            return false;
+        unordered_set<int> set(nums.begin(), nums.end());
+        return nums.size() != set.size();
+    }
+};
