b4d8d566b31ad88203295649816ddf63fbb3685a
cestlascorpion <cestlascorpion@gmail.com>
2018-03-29 21:58:27 +0800

    387. First Unique Character in a String

---
+++ 387. First Unique Character in a String.cpp
@@ -0,0 +1,26 @@
+#include "0. config.h"
+class Solution
+{
+  public:
+    int firstUniqChar(string s)
+    {
+        if (s.length() <= 0)
+            return -1;
+        unordered_map<char, int> hashmap;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (hashmap.find(s[i]) == hashmap.end())
+                hashmap[s[i]] = i;
+            else
+                hashmap[s[i]] = -1;
+        }
+        unordered_map<char, int>::iterator it = hashmap.begin();
+        int ans = -1;
+        while (it != hashmap.end())
+        {
+            if (it->second != -1)
+                ans = ans > it->second ? it->second : ans;
+        }
+        return ans;
+    }
+};
