ea022517e04e2d012a4ee6d81b18c3630a01e415
cestlascorpion <cestlascorpion@gmail.com>
2018-03-31 22:06:34 +0800

    （26-1）进制

---
+++ 168. Excel Sheet Column Title.cpp
@@ -0,0 +1,23 @@
+#include "0. config.h"
+
+class Solution
+{
+  public:
+    string convertToTitle(int n)
+    {
+        string res;
+        stack<char> stk;
+        while (n)
+        {
+            int d = (n - 1) % 26;
+            stk.push(d + 'A');
+            n = (n - 1) / 26;
+        }
+        while (!stk.empty())
+        {
+            res.push_back(stk.top());
+            stk.pop();
+        }
+        return res;
+    }
+};
