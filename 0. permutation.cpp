a549be12825ea74ce847d336af1d9fe73f3ff54f
cestlascorpion <cestlascorpion@gmail.com>
2018-04-01 20:00:25 +0800

    update

--- 0. permutation.cpp
+++ 0. permutation.cpp
@@ -2,28 +2,22 @@
 
 class Solution
 {
-public:
-  vector<vector<int>> premutatuion(vector<int> &nums)
-  {
-    int begin = 0;
-    int end = nums.size() - 1;
-    vector<vector<int>> ans;
-    if (begin >= end)
-      return ans;
-    for (int i = begin; i <= end; i++)
+    public:
+    vector<vector<int>> premutatuion(vector<int> &nums)
     {
-      if (isValid(nums, begin, i))
-      {
-        swap(nums[begin], nums[i]);
-        permutationHelper(nums, i, ans);
-        swap(nums[begin], nums[i]);
-      }
+        int begin = 0;
+        int end = nums.size() - 1;
+        vector<vector<int>> ans;
+        if (begin >= end)
+        return ans;
+        for (int i = begin; i <= end; i++)
+        {
+            swap(nums[begin], nums[i]);
+            permutationHelper(nums, i, ans);
+            swap(nums[begin], nums[i]);
+        }
     }
-  }
-  bool isValid(vector<int> &nums, int start, int index)
-  {
-    // return nums[start] != nums[index];
-    return true;
-  }
-  void permutationHelper(vector<int> &nums, int i, vector<vector<int>> &ans) {}
+    void permutationHelper(vector<int> &nums, int i, vector<vector<int>> &ans) 
+    {
+    }
 };
