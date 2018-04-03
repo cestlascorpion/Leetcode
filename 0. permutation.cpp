8db5cabc49521af1f04952bc3f3c6400b3103077
cestlascorpion <cestlascorpion@gmail.com>
2018-04-03 20:56:54 +0800

    update

---
+++ 0. permutation.cpp
@@ -0,0 +1,30 @@
+#include "0. config.h"
+
+class Solution
+{
+  public:
+    vector<vector<int>> premutatuion(vector<int> &nums)
+    {
+        int begin = 0;
+        int end = nums.size() - 1;
+        vector<vector<int>> ans;
+        if (begin >= end)
+            return ans;
+        premutatuionhelper(nums, begin, end, ans);
+        return ans;
+    }
+    void premutatuionhelper(vector<int> nums, int begin, int end, vector<vector<int>> &ans)
+    {
+        if (begin == end)
+        {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = begin; i <= end; i++)
+        {
+            swap(nums[begin], nums[i]);
+            premutatuionhelper(nums, begin + 1, end, ans);
+            swap(nums[begin], nums[i]);
+        }
+    }
+};
