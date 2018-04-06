c72833e84daacbb5cecbc3a2c1f370a1483fa9cb
cestlascorpion <cestlascorpion@gmail.com>
2018-04-06 19:01:09 +0800

    排序 / set / “链表环法”

---
+++ 287. Find the Duplicate Number.cpp
@@ -0,0 +1,53 @@
+#include "0. config.h"
+
+class Solution
+{
+  public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int ans = nums[0];
+        for (int i = 1; i < nums.size(); i++)
+            ans = ans ^ nums[i];
+        return ans;
+    }
+};
+class Solution2
+{
+  public:
+    int findDuplicate(vector<int> &nums)
+    {
+        set<int> s(nums.begin(), nums.end());
+        for (auto item : nums)
+        {
+            if (s.find(item) == s.end())
+                return item;
+            else
+                s.erase(item);
+        }
+        return -1;
+    }
+};
+class Solution3
+{
+  public:
+    int findDuplicate(vector<int> &nums)
+    {
+        if (nums.size() <= 0)
+            return -1;
+
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        fast = 0;
+        while (slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
