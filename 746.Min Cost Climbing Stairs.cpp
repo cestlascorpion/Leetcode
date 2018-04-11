8c0ca3fbd03c735197f0e4adeecdaa451f15b2e5
cestlascorpion <cestlascorpion@gmail.com>
2018-04-11 12:08:59 +0800

    update

--- 746.Min Cost Climbing Stairs.cpp
+++ 746.Min Cost Climbing Stairs.cpp
@@ -6,6 +6,28 @@ class Solution
     int minCostClimbingStairs(vector<int> &cost)
     {
         int ans = 0;
+        int pre1 = 0;
+        int pre2 = 0;
+        for (int i = 2; i < cost.size(); i++)
+        {
+            ans = min(pre1 + cost[i - 1], pre2 + cost[i - 2]);
+            pre2 = pre1;
+            pre1 = ans;
+        }
         return ans;
+    }
+    int minCostClimbingStairs2(vector<int> &cost)
+    {
+        vector<int> ans(cost.size(), 0);
+        for (int i = 2; i < cost.size(); i++)
+        {
+            ans[i] = min(ans[i - 1] + cost[i - 1], ans[i - 2] + cost[i - 2]);
+        }
+        /*
+        for (auto e : ans)
+            cout << e << "\t";
+        cout << endl;
+        */
+        return ans[cost.size() - 1];
     }
 };
