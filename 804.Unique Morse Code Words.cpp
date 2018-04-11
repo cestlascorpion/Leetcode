86f167f1552238c975254620cc0550e5751bb59c
cestlascorpion <cestlascorpion@gmail.com>
2018-04-11 16:19:49 +0800

    set

---
+++ 804.Unique Morse Code Words.cpp
@@ -0,0 +1,19 @@
+#include "000.config.h"
+
+class Solution
+{
+  public:
+    int uniqueMorseRepresentations(vector<string> &words)
+    {
+        vector<string> d = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        unordered_set<string> s;
+        for (auto word : words)
+        {
+            string code;
+            for (auto c : word)
+                code += d[c - 'a'];
+            s.insert(code);
+        }
+        return s.size();
+    }
+};
