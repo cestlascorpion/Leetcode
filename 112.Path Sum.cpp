92493e7d89cff3fa58bb66b0935c00d09a6a4aa2
cestlascorpion <cestlascorpion@gmail.com>
2018-04-11 17:18:43 +0800

    Path sum

---
+++ 112.Path Sum.cpp
@@ -0,0 +1,43 @@
+#include "000.config.h"
+
+class Solution
+{
+  public:
+    bool hasPathSum(TreeNode *root, int sum)
+    {
+        if (root == nullptr)
+            return false;
+
+        bool ans = false;
+        helper(root, sum, 0, ans);
+        return ans;
+    }
+    void helper(TreeNode *root, int &sum, int cur, bool &ans)
+    {
+        if (ans == true)
+            return;
+        cur += root->val;
+        if (!root->left && !root->right)
+        {
+            if (sum == cur)
+                ans = true;
+            return;
+        }
+        if (root->left)
+            helper(root->left, sum, cur, ans);
+        if (root->right)
+            helper(root->right, sum, cur, ans);
+    }
+};
+class Solution2
+{
+  public:
+    bool hasPathSum(TreeNode *root, int sum)
+    {
+        if (root == NULL)
+            return false;
+        if (root->val == sum && root->left == NULL && root->right == NULL)
+            return true;
+        return hasPathSum(root->left, sum - root->val) || hasPathSum(root->right, sum - root->val);
+    }
+}
