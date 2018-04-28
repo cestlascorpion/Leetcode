6493d8cd3725c938205d5bd2cf189da612b9fbe0
cestlascorpion <cestlascorpion@gmail.com>
2018-04-28 12:32:34 +0800

    update

---
+++ Leetcode334/Leetcode334.cpp
@@ -0,0 +1,13 @@
+//
+// Created by cestl on 2018/4/28 0028.
+//
+
+#include "Leetcode334.h"
+
+bool Leetcode334::increasingTriplet(vector<int> &nums) {
+    if (nums.size() < 3)
+        return false;
+
+    int min = INT32_MAX;
+    int m2n = INT32_MAX;
+}
---
+++ Leetcode334/Leetcode334.h
@@ -0,0 +1,16 @@
+//
+// Created by cestl on 2018/4/28 0028.
+//
+
+#ifndef LEETCODE_LEETCODE334_H
+#define LEETCODE_LEETCODE334_H
+
+#include "../config.h"
+
+class Leetcode334 {
+public:
+    bool increasingTriplet(vector<int>& nums);
+};
+
+
+#endif //LEETCODE_LEETCODE334_H
