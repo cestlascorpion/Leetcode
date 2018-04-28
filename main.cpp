61b5cf2dceace67028214e5f2a85fcdef0b3558a
cestlascorpion <cestlascorpion@gmail.com>
2018-04-28 22:13:08 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
-#include "config.h"
 #include "Leetcode200/Leetcode200.h"
+#include "config.h"
+
 
 using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    Leetcode200 sol;
-    sol.test200();
-    return 0;
+  std::cout << "Hello, World!" << std::endl;
+  Leetcode200 sol;
+  sol.test200();
+  return 0;
 }
